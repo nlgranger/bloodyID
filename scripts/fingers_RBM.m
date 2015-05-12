1344f3e8e2742244417250ac801c7e87ef8e4dbb
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-05-12 22:20:57 +0200

    toward a more stable architecture ?

--- scripts/fingers_RBM.m
+++ scripts/fingers_RBM.m
@@ -25,7 +25,6 @@ end
 
 %% Extraction layers
 
-extractionNet = MultiLayerNet(struct());
 RBMpretrainingOpts = struct(...
     'lRate', 0.01, ...
     'momentum', 0.6, ...
@@ -36,93 +35,67 @@ RBMpretrainingOpts = struct(...
     'wPenalty', 0.0001, ...
     'wDecayDelay', 10, ...
     'displayEvery', 5);
+%     'sparsity', 0.01, ...
+%     'sparseGain', 2, ...
 
-    %'sparsity', 0.01, ...
-    %'sparseGain', 2, ...
 RBMtrainOpts = struct( ...
-    'nIter', 0, ...
-    'batchSz', 60, ...
     'lRate', 0.04, ...
     'decayNorm', 2, ...
     'decayRate', 0.001);
 trainOpts = struct('nIter', 100, ...
-                   'batchSz', 50);
+                   'batchSz', 60);
 
+extractionNet = MultiLayerNet(struct());
+               
 % layer 1
 rbm = RBM(h*w, 200, RBMpretrainingOpts, RBMtrainOpts);
 extractionNet.add(rbm);
-extractionNet.pretrain(database.train_x);
 
 % layer 2
-% arch = struct('size', [200 100], ...
-%               'classifier',false, ...
-%               'inputType','binary');
-% rbm = RBM(arch, RBMpretrainingOpts, RBMtrainOpts);
-% extractionNet.add(rbm);
+rbm = RBM(200, 100, RBMpretrainingOpts, RBMtrainOpts);
+extractionNet.add(rbm);
 
-% layer 3
-% arch = struct('size', [500 100], ...
-%               'classifier',false, ...
-%               'inputType','binary');
-% rbm = RBM(arch, RBMpretrainingOpts, RBMtrainOpts);
-% extractionNet.add(rbm);
+extractionNet.pretrain(database.train_x);
 
 %% Comparison layers
 
 wholeNet   = MultiLayerNet(trainOpts);
-% Duplicate extraction network
-comparePreTrainOpts = struct('limit', Na);
-compareNet = CompareNet(extractionNet, 2, comparePreTrainOpts);
 reshapeNet = ReshapeNet(h*w*2, {h*w, h*w});
 wholeNet.add(reshapeNet);
+
+% Duplicate extraction network
+comparePreTrainOpts = struct('skip', true);
+compareNet = CompareNet(extractionNet, 2, comparePreTrainOpts);
 wholeNet.add(compareNet);
 
-% layer 3
-% arch = struct('size', [200 50], ...
-%               'classifier',false, ...
-%               'inputType','binary');
-% rbm = RBM(arch, RBMpretrainingOpts, RBMtrainOpts);
-reshapeNet = ReshapeNet(compareNet, 400);
+reshapeNet = ReshapeNet(compareNet, 200);
 wholeNet.add(reshapeNet);
-% wholeNet.add(rbm);
+
+% layer 3
+rbm = RBM(200, 100, RBMpretrainingOpts, RBMtrainOpts);
+wholeNet.add(rbm);
 
 % layer 4
-% RBMpretrainingOpts = {'verbose', 1, ...
-%     'lRate', 0.01, ...
-%     'momentum', 0.6, ...
-%     'nEpoch', 75, ...
-%     'wPenalty', 0.0001, ...
-%     'batchSz', 50, ...
-%     'nGibbs', 2, ...
-%     'displayEvery', 10};
-% arch = struct('size', [50 30], ...
-%               'classifier',false, ...
-%               'inputType','binary');
-% rbm = RBM(arch, RBMpretrainingOpts, RBMtrainOpts);
-% wholeNet.add(rbm);
-
-% layer 5
-% arch = struct('size', [30 1], ...
-%               'classifier',false, ...
-%               'inputType','binary');
-% rbm = RBM(arch, RBMpretrainingOpts, RBMtrainOpts);
-% wholeNet.add(rbm);
+rbm = RBM(100, 1, RBMpretrainingOpts, RBMtrainOpts);
+wholeNet.add(rbm);
 
 database = makepairs(database, nMatching, nNonMatching);
-Xa = wholeNet.compute(database.train_x);
-Xt = wholeNet.compute(database.test_x);
+wholeNet.pretrain(database.train_x);
 
-
-
-SVMModel = fitcsvm(Xa',database.train_y,'KernelFunction','rbf', 'OutlierFraction',0.05);
-y = predict(SVMModel,Xa');
-m = y ~= database.train_y;
-fprintf(1, 'Training fpr : %f\n', mean(m(~database.train_y)));
-fprintf(1, '         frr : %f\n', mean(m(database.train_y)));
-y = predict(SVMModel,Xt');
-m = y ~= database.test_y;
-fprintf(1, 'Testing  fpr : %f\n', mean(m(~database.test_y)));
-fprintf(1, '         frr : %f\n', mean(m(database.test_y)));
-
-% train
+%% Training
 % wholeNet.train(database.train_x, database.train_y);
+
+%% Testing
+
+% Xa = wholeNet.compute(database.train_x);
+% Xt = wholeNet.compute(database.test_x);
+% 
+% SVMModel = fitcsvm(Xa',database.train_y,'KernelFunction','rbf', 'OutlierFraction',0.05);
+% y = predict(SVMModel,Xa');
+% m = y ~= database.train_y;
+% fprintf(1, 'Training fpr : %f\n', mean(m(~database.train_y)));
+% fprintf(1, '         frr : %f\n', mean(m(database.train_y)));
+% y = predict(SVMModel,Xt');
+% m = y ~= database.test_y;
+% fprintf(1, 'Testing  fpr : %f\n', mean(m(~database.test_y)));
+% fprintf(1, '         frr : %f\n', mean(m(database.test_y)));
