cb2377b9d984ce224c8c67752063cccb64f75d0d
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-06-02 09:12:08 +0200

    more fixes, I hope it is correct now

--- data/hk_Qin_preprocessing/load_hk_Qin_preprocessing.m
+++ data/hk_Qin_preprocessing/load_hk_Qin_preprocessing.m
@@ -46,15 +46,9 @@ for d = 1:length(veinDirs)
     end
 end
 
-% Make pairs
-
-pairs     = cell(2,1);
-pairs{1}  = false(h, w, 0);
-pairs{2}  = false(h, w, 0);
-y         = false(1,0);
+% Assign individuals to training, validation and testing
 category  = zeros(numel(ids), 1, 'int32');
 
-% choose categories
 for id = 1:312
     fromS1 = find(ids == id & ses == 1, copies);
     fromS2 = find(ids == id & ses == 2, copies);
@@ -76,30 +70,43 @@ for id = 1:312
     end
 end
 
+% Make pairs
+pairs     = cell(2,1);
+paircat   = zeros(0, 1, 'int32');
+pairs{1}  = false(h, w, 0);
+pairs{2}  = false(h, w, 0);
+y         = false(1,0);
+
 for id = 1:210
     fromS1 = find(ids == id & ses == 1, copies);
     fromS2 = find(ids == id & ses == 2, copies);
     c = category(fromS1(1));
+    paircat = [paircat; repmat(c, numel(fromS1) * (nMatching+nNonMatching), 1)];
     
     nonpeers = find(ids ~= id & category == c);
     for i = 1: numel(fromS1)
         s = fromS1(i);
+        pairs{1} = cat(3, pairs{1}, ...
+                          repmat(x(:,:,s), 1, 1, nMatching + nNonMatching));
+        y        = [y; true(nMatching, 1); false(nNonMatching, 1)];
         shuffle  = randperm(copies, nMatching);
-        pairs{1} = cat(3, pairs{1}, repmat(x(:,:,s), 1, 1, nMatching));
         pairs{2} = cat(3, pairs{2}, x(:,:,fromS2(shuffle)));
-        y        = [y; true(nMatching, 1)];
         shuffle  = randperm(numel(nonpeers), nNonMatching);
-        pairs{1} = cat(3, pairs{1}, repmat(x(:,:,s), 1, 1, nNonMatching));
         pairs{2} = cat(3, pairs{2}, x(:,:, nonpeers(shuffle)));
-        y        = [y; false(nNonMatching, 1)];
     end
 end
 
-dataset.pretrain_x = x(:,:, category == 4);
-dataset.train_x    = {pairs{1}(:,:, category == 1); pairs{2}(:,:, category == 1)};
-dataset.train_y    = y(category == 1);
-dataset.val_x      = {pairs{1}(:,:, category == 2); pairs{2}(:,:, category == 2)};
-dataset.val_y      = y(category == 2);
-dataset.test_x     = {pairs{1}(:,:, category == 3); pairs{2}(:,:, category == 3)};
-dataset.test_y     = y(category == 3);
+% Build dataset
+
+dataset.x          = x;
+dataset.category   = category;
+dataset.id         = ids;
+dataset.ses        = ses;
+dataset.pretrain_x = x(:,:, (category == 1) | (category == 4));
+dataset.train_x    = {pairs{1}(:,:, paircat == 1); pairs{2}(:,:, paircat == 1)};
+dataset.train_y    = y(paircat == 1);
+dataset.val_x      = {pairs{1}(:,:, paircat == 2); pairs{2}(:,:, paircat == 2)};
+dataset.val_y      = y(paircat == 2);
+dataset.test_x     = {pairs{1}(:,:, paircat == 3); pairs{2}(:,:, paircat == 3)};
+dataset.test_y     = y(paircat == 3);
 end
