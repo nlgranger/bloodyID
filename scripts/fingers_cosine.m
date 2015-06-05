2f9f6471236cbabc59cc4ab4594debe9d9a7f0ab
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-06-05 11:23:06 +0200

    trying different parameters

--- scripts/fingers_cosine.m
+++ scripts/fingers_cosine.m
@@ -1,9 +1,9 @@
-h = 40;
-w = 100;
+h = 32;
+w = 75;
 nMatching     = 3; % number of matching pairs for each image
-nNonMatching  = 3; % number of non matching pairs for each image
-patchSz       = [20 25];
-overlap       = [10 12];
+nNonMatching  = 6; % number of non matching pairs for each image
+patchSz       = [15 20];
+overlap       = [11 16];
 
 %% load initial data
 
@@ -19,17 +19,38 @@ load('data/hk_Qin_preprocessing/dataset.mat');
 
 %% Extraction layers
 
-RBMtrainOpts = struct('lRate', 100, ...
-                      'decayNorm', 2, ...
-                      'decayRate', 1e-10);
-
 extractionNet = MultiLayerNet(struct('skipBelow', 1));
 
 patchMaker = PatchNet([h, w], patchSz, overlap);
 extractionNet.add(patchMaker);
 
 patchRedux = MultiLayerNet(struct());
+RBMtrainOpts = struct('lRate', 50);
+RBMpretrainingOpts = struct( ...
+    'lRate', 0.05, ...
+    'momentum', 0.9, ...
+    'nEpochs', 125, ...
+    'batchSz', 300, ...
+    'dropoutVis', 0.4, ...
+    'wPenalty', 0.005, ...
+    'wDecayDelay', 0, ...
+    'sparsity', 0.05, ...
+    'sparseGain', 1, ...
+    'displayEvery', 5);
+rbm = RBM(prod(patchSz), 70, RBMpretrainingOpts, RBMtrainOpts);
+patchRedux.add(rbm);
 
+RBMtrainOpts = struct('lRate', 5);
+rbm = RBM(70, 25, RBMpretrainingOpts, RBMtrainOpts);
+patchRedux.add(rbm);
+
+imRedux = SiameseNet(patchRedux, numel(patchMaker.outsize()));
+extractionNet.add(imRedux);
+
+patchMerge = ReshapeNet(imRedux, sum(cellfun(@prod, imRedux.outsize())));
+extractionNet.add(patchMerge);
+
+RBMtrainOpts = struct('lRate', 5);
 RBMpretrainingOpts = struct( ...
     'lRate', 0.05, ...
     'momentum', 0.9, ...
@@ -38,34 +59,14 @@ RBMpretrainingOpts = struct( ...
     'dropoutVis', 0.4, ...
     'wPenalty', 0.005, ...
     'wDecayDelay', 0, ...
-    'selectivity', 0.1, ...
-    'selectivityGain', 0.5, ...
     'sparsity', 0.05, ...
     'sparseGain', 0.1, ...
     'displayEvery', 5);
-rbm = RBM(prod(patchSz), 200, RBMpretrainingOpts, RBMtrainOpts);
-patchRedux.add(rbm);
-RBMpretrainingOpts = struct( ...
-    'lRate', 0.05, ...
-    'momentum', 0.9, ...
-    'nEpochs', 180, ...
-    'batchSz', 300, ...
-    'dropoutVis', 0.2, ...
-    'wPenalty', 0.01, ...
-    'wDecayDelay', 10, ...
-    'sparsity', 0.05, ...
-    'sparseGain', 0.1, ...
-    'displayEvery', 5);
-rbm = RBM(200, 100, RBMpretrainingOpts, RBMtrainOpts);
-patchRedux.add(rbm);
-rbm = RBM(100, 50, RBMpretrainingOpts, RBMtrainOpts);
-patchRedux.add(rbm);
-
-imRedux = SiameseNet(patchRedux, numel(patchMaker.outsize()));
-extractionNet.add(imRedux);
-
-patchMerge = ReshapeNet(imRedux, sum(cellfun(@prod, imRedux.outsize())));
-extractionNet.add(patchMerge);
+rbm = RBM(25 * numel(patchMaker.outsize()), 300, RBMpretrainingOpts, RBMtrainOpts);
+extractionNet.add(rbm);
+RBMtrainOpts = struct('lRate', 1);
+rbm = RBM(300, 150, RBMpretrainingOpts, RBMtrainOpts);
+extractionNet.add(rbm);
 
 extractionNet.pretrain(dataset.pretrain_x);
 
@@ -77,25 +78,34 @@ trainOpts = struct('nIter', 150, ...
                    'batchSz', 300, ...
                    'displayEvery', 5);
 
-wholeNet   = MultiLayerNet(trainOpts);
+wholeNet  = MultiLayerNet(trainOpts);
 
 % Duplicate extraction network
 compareNet = SiameseNet(extractionNet, 2, 'skipPretrain');
 wholeNet.add(compareNet);
 
-cosine = CosineCompare(50);
+cosine = CosineCompare(100);
 wholeNet.add(cosine);
 
 %% Training
-% wholeNet.pretrain(dataset.train_x);
-wholeNet.train(dataset.train_x, dataset.train_y');
 
 o = wholeNet.compute(dataset.test_x);
-m = o > 0.42 ~= dataset.test_y';
+m = o > 0.47 ~= dataset.test_y';
 mean(m(dataset.test_y))
 mean(m(~dataset.test_y))
 o = wholeNet.compute(dataset.train_x);
-m = o > .42 ~= dataset.train_y';
+m = o > .47 ~= dataset.train_y';
+mean(m(dataset.train_y))
+mean(m(~dataset.train_y))
+
+wholeNet.train(dataset.train_x, dataset.train_y');
+
+o = wholeNet.compute(dataset.test_x);
+m = o > 0.44 ~= dataset.test_y';
+mean(m(dataset.test_y))
+mean(m(~dataset.test_y))
+o = wholeNet.compute(dataset.train_x);
+m = o > .44 ~= dataset.train_y';
 mean(m(dataset.train_y))
 mean(m(~dataset.train_y))
 
