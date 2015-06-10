7f0da14d9be2e2da2bbb015f5ca8af02ac7052eb
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-06-10 11:22:05 +0200

    backup

--- data/hk_original/fingerExtraction.m
+++ data/hk_original/fingerExtraction.m
@@ -1,20 +1,23 @@
 function [O, M] = fingerExtraction(I)
+patchSize = 40;
+ratio = 2;
 [h, w] = size(I);
-% Center image
-g = fspecial('gaussian', [40 30], 8);
-e = fspecial('gaussian', 10, 4);
+g = fspecial('gaussian', [30 20], 5);
+e = fspecial('gaussian', 15, 15);
+p = fspecial('gaussian',[patchSize patchSize], patchSize);
 
-B = 255-imfilter(I, g);
-m = mean(mean(B));
-M = B > m;
-[y, x] = find(M);
+% Center and align image
+B = imfilter(I, g);
+m = mean(mean(B(100:end-100, 25:end-25)));
+M = B < m;
+[y, x] = find(M(100:end-100, 25:end-25));
 tmp    = cov(x,y);
 angle  = atan(tmp(2,1)/var(x));
 O      = imrotate(I, 180*angle/pi, 'bilinear');
 M      = imrotate(M, 180*angle/pi, 'bilinear');
 A      = imrotate(true(h,w), 180*angle/pi, 'bilinear');
-[y, ~] = find(M);
-off    = round(size(O,1)/2-mean(y));
+[y, ~] = find(M(100:end-100, 25:end-25));
+off    = round(size(O,1)/2-mean(y))+25;
 if off > 0
     O = [zeros(off, size(O, 2)); O(1:end-off, :)];
     A = [zeros(off, size(O, 2)); A(1:end-off, :)];
@@ -35,11 +38,14 @@ M = B > min(min(B)) + 10 & B < max(max(B)) - 10 & A ...
 
 % Find edges
 G = imgradient(B);
+G = tanh(std(reshape(G, numel(G), 1)));
+bg = imfilter(G, p);
+G = G - bg;
 m = mean(G(M));
 s = std(G(M));
-U = (G > m + s/6);
+U = (G > m);
 M = U | ~M;
-M = ~imclose(M, strel('disk', 5'));
+M = ~imopen(M, strel('disk', 5'));
 
 % Keep finger areas (along the horizon)
 [L,n] = bwlabel(M, 4);
@@ -53,11 +59,61 @@ for k = 1:n
     end
 end
 
-M = imclose(M, strel('disk', 6));
+M = imclose(M, strel('disk', 8));
 M = imfill(M, 'holes');
 
-% Find ROI
-hist = filter2(ones(1,40), sum(O .* uint8(M)));
-find
+if any(sum(M(:,100:end-100))==0)
+    M = false;
+    O = [];
+    return
+end
 
+% Find ROI
+width = sum(M);
+width(width < 50) = -1;
+his   = filter2(ones(1,40), sum(O .* uint8(M)));
+dipj   = find((his(2:end-1) > 10000) ...
+    & abs(.5 * (his(1:end-2) - his(3:end))) < 300, ...
+    1, 'last') + 1;
+if numel(dipj) == 0 || dipj <= 250 || dipj >= 473
+    M = false;
+    O = [];
+    return
+end
+
+[y, x] = find(M(:, dipj-250:dipj+50));
+tmp    = cov(x,y);
+angle2 = atan(tmp(2,1)/var(x));
+O      = imrotate(O, 180*angle2/pi, 'bilinear');
+M      = imrotate(M, 180*angle2/pi, 'bilinear');
+h      = round(sum(sum(M(:, dipj-250:dipj+50)))/600);
+w      = round(ratio*h*2/5);
+m      = round(mean(sum(bsxfun(@times, ...
+                               M(:, dipj-250:dipj+50), ...
+                               (1:size(M, 1))')) ...
+                    ./ (sum(M(:, dipj-250:dipj+50))+1)));
+                
+% Remove low freq colors
+% b    = ones(2*patchSize+1);
+% bg   = conv2(double(O), b, 'same');
+% d    = conv2(double(M), b, 'same');
+% d(d==0) = 1;
+% bg      = bg ./ d;
+O  = double(O);
+bg = ones(size(O)) * mean(O(M));
+bg(M) = O(M);
+bg = filter2(p, bg);
+O       = O - bg;
+
+% Cut ROI and resize
+M = M(m-h:m+h, dipj-4*w:dipj+w);
+O = O(m-h:m+h, dipj-4*w:dipj+w);
+M = imresize(M, [100 200]);
+O = imresize(O, [100 200]);
+
+% Spread histogram
+mini  = min(O(M));
+maxi  = max(O(M));
+O     = (O-mini)*255/(maxi-mini);
+O(~M) = 0;
 end
