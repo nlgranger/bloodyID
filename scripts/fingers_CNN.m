21f2b9826dd3b6ca108356c32facd03b08f5e5ec
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-07-15 15:17:32 +0200

    rename dataset files, added classification perf monitoring every 25 loops, initializing cnn with gabor filters, baseline needs more work

--- scripts/fingers_CNN.m
+++ scripts/fingers_CNN.m
@@ -9,14 +9,15 @@ nFolds       = 7;
 
 %% load initial data
 % CNN requires single precision input!
-if exist('data/hk_original/dataset.mat', 'file')
-    load('data/hk_original/dataset.mat');
+if exist('data/hk_original/dataset_small.mat', 'file')
+    load('data/hk_original/dataset_small.mat');
 else
     dataset = make_dataset('data/hk_original', [35 81], [1 0], [4 6], ...
-        'preprocessed', 'data/hk_original/preprocessed.mat', ...
+        'preprocessed', 'data/hk_original/preprocessed_small.mat', ...
+        'nExtra', 1, ...
         'nFolds', nFolds);
     dataset.X = -single(dataset.X);
-    save('data/hk_original/dataset.mat', 'dataset');
+    save('data/hk_original/dataset_small.mat', 'dataset');
 end
 
 %% Extraction layers
@@ -24,12 +25,20 @@ end
 extractionNet = MultiLayerNet(struct());
 
 trainOpts = struct('lRate', 2e-6);
-cnn = CNN([h w], [8 10], 24, trainOpts, 'pool', [4 4], 'dropout', 0.3);
+cnn = CNN([h w], [8 10], 16, trainOpts, 'pool', [4 4], 'dropout', 0.4);
+
+L = [15, 18];
+for j = 1:numel(L)
+    l = L(j);
+    for k = 1:4
+        cnn.filters(:,:,1,(j-1)*4+k) = gaborfilter([8 10], [5 5], l, k * pi/4) / (8*10*16);
+    end
+end
 extractionNet.add(cnn);
 
 trainOpts = struct('lRate', 2e-6);
-outSz = extractionNet.outsize();
-cnn   = CNN(outSz, [2 3], 5, trainOpts, 'pool', [2 2], 'dropout', 0.2);
+outSz     = extractionNet.outsize();
+cnn       = CNN(outSz, [2 3], 8, trainOpts, 'pool', [2 2], 'dropout', 0.2);
 extractionNet.add(cnn);
 
 concat = ReshapeNet(extractionNet, prod(extractionNet.outsize()));
@@ -37,47 +46,52 @@ extractionNet.add(concat);
 
 rbm = RELURBM(extractionNet.outsize(), 60, struct(), trainOpts);
 extractionNet.add(rbm);
+rbm = RELURBM(extractionNet.outsize(), 60, struct(), trainOpts);
+extractionNet.add(rbm);
 
 %% Comparison layers
 
-trainOpts  = struct('nIter', 60, ...
-                    'batchSz', 300, ...
-                    'displayEvery', 5);
+trainOpts  = struct('nIter', 25, ...
+                    'batchSz', 500, ...
+                    'displayEvery', 10);
 wholeNet   = MultiLayerNet(trainOpts);
-compareNet = SiameseNet(extractionNet, 2, 'skipPretrain');
+compareNet = SiameseNet(extractionNet, 2);
 wholeNet.add(compareNet);
 
-l2 = L2Compare(120);
-wholeNet.add(l2);
+cosine = CosineCompare(120);
+wholeNet.add(cosine);
 
 %% Training
 
-res = zeros(nFolds, 4);
+res = zeros(nFolds, 4, 4);
 
-for i = 1:1%nFolds
+for i = 1:nFolds
     X = {dataset.X(:,:,dataset.train_x{i}(:,1)), dataset.X(:,:,dataset.train_x{i}(:,2))};
-    Y = 3*(~dataset.train_y{i})';
+    Y = (~dataset.train_y{i})';
     
     net = wholeNet.copy();
-    net.train(X, Y);
-
-    r = zeros(1,4);
-    o = net.compute(X)';
-    eer = fminsearch(@(t) abs(mean(o(dataset.train_y{i})<t) - mean(o(~dataset.train_y{i})>t)), 1.5);
-    m = (o > eer) == dataset.train_y{i};
-    r(1) = mean(m(dataset.train_y{i}));
-    r(2) = mean(m(~dataset.train_y{i}));
-
-    X = {dataset.X(:,:,dataset.val_x{i}(:,1)), dataset.X(:,:,dataset.val_x{i}(:,2))};
-    Y = 3*(~dataset.val_y{i})';
     
-    o = net.compute(X)';
-    m = (o > eer) == dataset.val_y{i};
-    r(3) = mean(m(dataset.val_y{i}));
-    r(4) = mean(m(~dataset.val_y{i}));
-    res(i,:) = r;
-    disp(i);
-    disp(r);
+    for j = 1:4
+        net.train(X, Y);
+
+        r = zeros(1, 4);
+        o = net.compute(X)';
+        eer = fminsearch(@(t) abs(mean(o(dataset.train_y{i})<t) - mean(o(~dataset.train_y{i})>t)), 1.5);
+        m = (o > eer) == dataset.train_y{i};
+        r(1) = mean(m(dataset.train_y{i}));
+        r(2) = mean(m(~dataset.train_y{i}));
+
+        Xv = {dataset.X(:,:,dataset.val_x{i}(:,1)), dataset.X(:,:,dataset.val_x{i}(:,2))};
+        o = net.compute(Xv)';
+        clear Xv;
+        
+        m = (o > eer) == dataset.val_y{i};
+        r(3) = mean(m(dataset.val_y{i}));
+        r(4) = mean(m(~dataset.val_y{i}));
+        res(i,:,j) = r;
+        disp(i);
+        disp(r);
+    end
 end
 
 disp(res);
--- scripts/fingers_baseline.m
+++ scripts/fingers_baseline.m
@@ -7,36 +7,36 @@ nNonMatching = 6; % number of non matching pairs for each image
 coeff        = 0.4;% this proportion of the pixels should match
 
 %% load initial data
-if exist('data/hk_original/dataset.mat', 'file')
-    load('data/hk_original/dataset.mat');
+if exist('data/hk_original/dataset_big.mat', 'file')
+    load('data/hk_original/dataset_big.mat');
 else
     dataset = make_dataset('data/hk_original', [h w], [1 0], [4 6], ...
-        'preprocessed', 'data/hk_original/preprocessed.mat');
+        'preprocessed', 'data/hk_original/preprocessed_big.mat');
     X = dataset.X;
     dataset.X = false([h w size(X, 3)]);
     for i = 1:size(dataset.X, 3)
         dataset.X(:,:,i) = vein_extraction(X(:,:,i)) ...
             .* dataset.M(:,:,i);
     end
-    save('data/hk_original/dataset.mat', 'dataset');
+    save('data/hk_original/dataset_big.mat', 'dataset');
 end
 
 %% Testing
-optimizer = registration.optimizer.OnePlusOneEvolutionary;
-metric    = registration.metric.MattesMutualInformation;
-optimizer.InitialRadius = 0.001;
-optimizer.Epsilon = 1.5e-4;
-optimizer.GrowthFactor = 1.005;
+optimizer                   = registration.optimizer.OnePlusOneEvolutionary;
+metric                      = registration.metric.MattesMutualInformation;
+optimizer.InitialRadius     = 0.001;
+optimizer.Epsilon           = 1.5e-4;
+optimizer.GrowthFactor      = 1.005;
 optimizer.MaximumIterations = 70;
 
 res = zeros(numel(dataset.train_y), 3);
-for i = 1:numel(dataset.train_y)
+
+pool = parpool();
+parfor i = 1:numel(dataset.train_y)
     I1 = dataset.X(:,:,dataset.train_x(i,1));
     I2 = dataset.X(:,:,dataset.train_x(i,2));    
     Ir = imregister(uint8(I1), uint8(I2), 'rigid', optimizer, metric);
-    res(i, 1) = sum(sum(I2));
-    res(i, 2) = sum(sum(Ir));
-    res(i, 3) = sum(sum(I2 & Ir));
+    res(i, :) = [sum(sum(I2)), sum(sum(Ir)), sum(sum(I2 & Ir))];
         
 %     subplot(3,1,1)
 %     imagesc(I1); axis image; axis off;
@@ -46,3 +46,4 @@ for i = 1:numel(dataset.train_y)
 %     imshowpair(Ir, uint8(I2));
 %     pause
 end
+delete(pool);
