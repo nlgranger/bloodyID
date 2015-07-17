4aceb070e33ffe6593cdf6801694370a26a4811f
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-07-17 16:03:32 +0200

    saving before reworking architecture

--- scripts/fingers_CNN.m
+++ scripts/fingers_CNN.m
@@ -14,7 +14,6 @@ if exist('data/hk_original/dataset_small.mat', 'file')
 else
     dataset = make_dataset('data/hk_original', [35 81], [1 0], [4 6], ...
         'preprocessed', 'data/hk_original/preprocessed_small.mat', ...
-        'nExtra', 1, ...
         'nFolds', nFolds);
     dataset.X = -single(dataset.X);
     save('data/hk_original/dataset_small.mat', 'dataset');
@@ -24,41 +23,41 @@ end
 
 extractionNet = MultiLayerNet(struct());
 
-trainOpts = struct('lRate', 2e-6);
-cnn = CNN([h w], [8 10], 16, trainOpts, 'pool', [4 4], 'dropout', 0.4);
+trainOpts = struct('lRate', 3e-6);
+cnn = CNN([h w], [12 14], 16, trainOpts, 'pool', [4 4], 'dropout', 0.4);
 
 L = [15, 18];
 for j = 1:numel(L)
     l = L(j);
     for k = 1:4
-        cnn.filters(:,:,1,(j-1)*4+k) = gaborfilter([8 10], [5 5], l, k * pi/4) / (8*10*16);
+        cnn.filters(:,:,1,(j-1)*4+k) = ...
+            gaborfilter([12 14], [5 5], l, k * pi/4) / (8*10*16);
     end
 end
 extractionNet.add(cnn);
 
-trainOpts = struct('lRate', 2e-6);
+trainOpts = struct('lRate', 3e-6);
 outSz     = extractionNet.outsize();
-cnn       = CNN(outSz, [2 3], 8, trainOpts, 'pool', [2 2], 'dropout', 0.2);
+cnn       = CNN(outSz, [3 6], 8, trainOpts, 'pool', [2 2], 'dropout', 0.2);
 extractionNet.add(cnn);
 
 concat = ReshapeNet(extractionNet, prod(extractionNet.outsize()));
 extractionNet.add(concat);
 
-rbm = RELURBM(extractionNet.outsize(), 60, struct(), trainOpts);
-extractionNet.add(rbm);
-rbm = RELURBM(extractionNet.outsize(), 60, struct(), trainOpts);
+trainOpts = struct('lRate', 3e-7);
+rbm = RELURBM(extractionNet.outsize(), 50, struct(), trainOpts);
 extractionNet.add(rbm);
 
 %% Comparison layers
 
 trainOpts  = struct('nIter', 25, ...
                     'batchSz', 500, ...
-                    'displayEvery', 10);
+                    'displayEvery', 5);
 wholeNet   = MultiLayerNet(trainOpts);
 compareNet = SiameseNet(extractionNet, 2);
 wholeNet.add(compareNet);
 
-cosine = CosineCompare(120);
+cosine = CosineCompare(100);
 wholeNet.add(cosine);
 
 %% Training
@@ -76,7 +75,7 @@ for i = 1:nFolds
 
         r = zeros(1, 4);
         o = net.compute(X)';
-        eer = fminsearch(@(t) abs(mean(o(dataset.train_y{i})<t) - mean(o(~dataset.train_y{i})>t)), 1.5);
+        eer = fminsearch(@(t) abs(mean(o(dataset.train_y{i})<t) - mean(o(~dataset.train_y{i})>t)), 0.5);
         m = (o > eer) == dataset.train_y{i};
         r(1) = mean(m(dataset.train_y{i}));
         r(2) = mean(m(~dataset.train_y{i}));
@@ -95,11 +94,3 @@ for i = 1:nFolds
 end
 
 disp(res);
-
-% colormap gray
-% W = wholeNet.nets{1}.net.nets{1}.filters;
-% for i = 1:size(W, 4)
-%     imagesc(W(:,:,1,i));
-%     axis image;
-%     pause
-% end
