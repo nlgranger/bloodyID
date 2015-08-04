7d62321e233b7e4fa60a272742b3c529314c124e
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2015-08-04 13:50:52 +0200

    stable architecture (poor results though)

--- scripts/fingers_ReLU.m
+++ scripts/fingers_ReLU.m
@@ -11,94 +11,105 @@ nFolds       = 7;
 
 %% load initial data
 
-if exist('data/hk_original/dataset_small.mat', 'file')
-    load('data/hk_original/dataset_small.mat');
+if exist('data/hk_original/dataset_small_pos.mat', 'file')
+    load('data/hk_original/dataset_small_pos.mat');
 else
     dataset = make_dataset('data/hk_original', ...
         [h w], [0.5 0], [nMatching nNonMatching], ...
         'preprocessed', 'data/hk_original/preprocessed_small.mat', ...
         'nFolds', nFolds);
     dataset.X = 1 - 0.5 * (dataset.X + 1);
-    save('data/hk_original/dataset_small.mat', 'dataset');
+    save('data/hk_original/dataset_small_pos.mat', 'dataset');
 end
 
 %% Extraction layers
 
-extractionNet = MultiLayerNet();
-
-patchMaker = PatchNet([h, w], patchSz, overlap);
-extractionNet.add(patchMaker);
-extractionNet.freezeBelow(1);
-
-RBMtrainOpts = struct( ...
-    'lRate', 2e-4, ...
-    'dropout', 0.2);
-RBMpretrainingOpts = struct( ...
-    'lRate', 2e-5, ...
+% Patch filter RBM
+RBM1pretrainingOpts = struct( ...
+    'lRate', 1e-5, ...
     'momentum', 0.5, ...
-    'nEpochs', 120, ...
+    'nEpochs', 60, ...
     'batchSz', 400, ...
     'displayEvery', 10);
-rbm = RELURBM(prod(patchSz), 150, RBMpretrainingOpts, RBMtrainOpts, false);
+RBM1trainOpts = struct( ...
+    'lRate', 2e-4, ...
+    'dropout', 0.2);
 
-imRedux = SiameseNet(rbm, numel(patchMaker.outsize()));
-extractionNet.add(imRedux);
-
-patchMerge = ReshapeNet(imRedux, sum(cellfun(@prod, imRedux.outsize())));
-extractionNet.add(patchMerge);
-
-RBMtrainOpts = struct( ...
-    'lRate', 2e-4);
-RBMpretrainingOpts = struct( ...
-    'lRate', 2e-5, ...
+% fusion RBM
+RBM2pretrainingOpts = struct( ...
+    'lRate', 1e-5, ...
     'momentum', 0.5, ...
     'nEpochs', 30, ...
     'batchSz', 400, ...
     'displayEvery', 5);
-rbm = RELURBM(150 * numel(patchMaker.outsize()), 150, RBMpretrainingOpts, RBMtrainOpts, true);
-extractionNet.add(rbm);
+RBM2trainOpts = struct( ...
+    'lRate', 2e-4);
 
 %% Training
 
-trainOpts = struct('nIter', 100, ...
-                   'batchSz', 400, ...
-                   'batchFn', @pairsBatchFn, ...
-                   'displayEvery', 3);
+trainOpts = struct(...
+    'nIter', 100, ...
+    'batchSz', 400, ...
+    'batchFn', @pairsBatchFn, ...
+    'displayEvery', 3);
 
-for i = 1:1%nFolds
-    extraction = extractionNet.copy();
-%     extraction.pretrain(dataset.X(:,:, ...
-%         unique([dataset.pretrain_x; ...
-%                 dataset.train_x{i}(:,1); ...
-%                 dataset.train_x{i}(:,2)])));
-    rbm = RELURBM(150, 100, RBMpretrainingOpts, RBMtrainOpts, true);
-    extraction.add(rbm);
+for i = 1:1%nFolds % Cross-validation loop
+    extractionNet = MultiLayerNet();
+    
+    % Filter layers
+    patchMaker    = PatchNet([h, w], patchSz, overlap);
+    extractionNet.add(patchMaker);
+    extractionNet.freezeBelow(1);% don't train patch extraction
+    
+    rbm = RELURBM(prod(patchSz), 150, ...
+        RBM1pretrainingOpts, RBM1trainOpts, false);
+    imRedux = SiameseNet(rbm, numel(patchMaker.outsize()));
+    extractionNet.add(imRedux);
+    
+    % Pretraining
+    pretrainIdx = unique([dataset.pretrain_x; ...
+                dataset.train_x{i}(:,1); ...
+                dataset.train_x{i}(:,2)]);
+    extractionNet.pretrain(dataset.X(:,:, pretrainIdx));
+    
+    patchMerge = ReshapeNet(extractionNet, ...
+        sum(cellfun(@prod, extractionNet.outsize())));
+    extractionNet.add(patchMerge);
 
-    compareNet = SiameseNet(extraction, 2, 'skipPretrain');
+    % Dimension reduction layers
+    rbm = RELURBM(extractionNet.outsize(), 150, ...
+        RBM2pretrainingOpts, RBM2trainOpts, true);
+    extractionNet.add(rbm);
+    rbm = RELURBM(extractionNet.outsize(), 100, ...
+        RBM2pretrainingOpts, RBM2trainOpts, true);
+    extractionNet.add(rbm);
+    
+    % Combined network
+    compareNet = SiameseNet(extractionNet, 2);
     wholeNet   = MultiLayerNet();
-    metric     = CosineCompare(100);
+    metric     = L2Compare(100);
     wholeNet.add(compareNet);
     wholeNet.add(metric);
     
+    % Training
     X = struct('data', dataset.X, 'pairs', dataset.train_x{i});
     Y = 2*(~dataset.train_y{i})';
-    net = wholeNet.copy(); % start from random weights
-    train(net, @L2Cost, X, Y, trainOpts);
+    train(wholeNet, @L2Cost, X, Y, trainOpts);
     r = zeros(1, 4);
     
-    % Compute mis-classification on training data
+    % Training performances
     [allX, allY] = trainOpts.batchFn(X, Y, inf, []);
-    o = net.compute(allX);
+    o = wholeNet.compute(allX);
     eer = fminsearch(@(t) abs(mean(o(allY == 0)<t) - mean(o(allY > 0)>=t)), 1);
     m = (o > eer) ~= (allY > 0);
     r(1) = mean(m(allY > 0));
     r(2) = mean(m(allY == 0));
 
-    % Compute mis-classification on testing data
+    % Validation performances
     X = struct('data', dataset.X, 'pairs', dataset.val_x{i});
     Y = single((~dataset.val_y{i}))';
     [allX, allY] = trainOpts.batchFn(X, Y, inf, []);
-    o = net.compute(allX);
+    o = wholeNet.compute(allX);
     m = (o > eer) ~= (allY > 0);
     r(3) = mean(m(allY > 0));
     r(4) = mean(m(allY == 0));
